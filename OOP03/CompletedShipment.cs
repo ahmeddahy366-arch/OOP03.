c0734087112896d42fdfe7cdc7a9522adde3ef25
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-08-30 18:01:50 +0300

    Question 08

---
+++ OOP03/CompletedShipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP03
+{
+    #region Question 08
+
+    internal class CompletedShipment:Shipment
+    {
+        public CompletedShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("Completed Shipment (Sealed Class)");
+            base.PrintShipment();
+        }
+    }
+}
+    #endregion
