5101daf71646eb70c33dd6f6ef7f3a12d5dffa84
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-08-30 17:07:06 +0300

    question2

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -18,6 +18,20 @@
             //static Binding ->Early Binding = Compile Time = Reference Type =overloading
             //dynamic Binding ->Late Binding = Run Time    = Actual Object = overriding
             #endregion
+            #region question2
+
+            //Sealed Classes and Methods
+            //a)  What is the purpose of the sealed keyword when applied to a class?
+            //    A sealed class cannot be inherited.
+
+            //b)  What is the difference between a sealed class and a sealed method?
+            //        A sealed class prevents inheritance, while a sealed method prevents further overriding of that method.
+
+
+            //c)  Can a sealed method be overridden? Why?
+            //No A sealed method cannot be overridden because sealed prevents further overriding in derived classes.
+            #endregion
+
         }
     }
 }
