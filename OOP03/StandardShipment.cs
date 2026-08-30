90af91a48cf75ef6afd303cda3854a9d4a881a47
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-08-30 17:38:09 +0300

    Question 02

---
+++ OOP03/StandardShipment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP03
+{
+    #region Question 02
+    internal class StandardShipment:Shipment
+    {
+        public StandardShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+        }
+
+        public override void PrintShipment()
+        {
+            Console.WriteLine("Standard Shipment");
+            Console.WriteLine("------------------------------------------");
+            base.PrintShipment();
+        }
+    }
+          #endregion
+
+}
