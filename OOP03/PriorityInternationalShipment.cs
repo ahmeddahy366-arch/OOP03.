76b240ebc8f7d0dc10d970a84bc8a1f865a187d0
Ahmed El-dahy <ahmeddahy366-arch@gmail.com>
2026-08-30 18:03:27 +0300

    Question 09

---
+++ OOP03/PriorityInternationalShipment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP03
+{
+    #region Question 09
+
+    internal class PriorityInternationalShipment : InternationalShipment
+    {
+        public PriorityInternationalShipment(
+            string trackingCode,
+            string description,
+            decimal weight,
+            decimal deliveryFee,
+            string destinationCountry,
+            decimal customsFee,
+            DeliveryAddress destination)
+            : base(
+                trackingCode,
+                description,
+                weight,
+                deliveryFee,
+                destinationCountry,
+                customsFee,
+                destination)
+        {
+        }
+
+        public sealed override void GenerateCustomsReport()
+        {
+            Console.WriteLine("Priority customs report generated.");
+        }
+    }
+}
+    #endregion
